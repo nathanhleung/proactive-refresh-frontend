8932d3d4c1a353c4262f82b37b16a3b1e1d41bf2
nathanhleung <github@nathanhleung.com>
2023-02-17 22:38:37 -0800

    chore: add safe-react-components providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@safe-global/safe-react-components/dist/fonts.css";
+import { Theme, ThemeProvider } from "@mui/material/styles";
+import { SafeThemeProvider } from "@safe-global/safe-react-components";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SafeThemeProvider mode="light">
+      {(safeTheme: Theme) => (
+        <ThemeProvider theme={safeTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      )}
+    </SafeThemeProvider>
+  );
 }
