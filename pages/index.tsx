0cdc35a46b517c70d5fdbcc665f329f8535cd80b
straightupjac <jdc.jaclyn@gmail.com>
2023-02-18 18:03:46 -0800

    copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,24 +21,9 @@ export default function Home() {
             Enterprise-grade custody and authentication.
             <Text fontSize={24} fontWeight={300} pt={2}>
               The safest and most secure way to transfer funds, sign messages,
-              and protect what&apos;s most important to your organization.
-            </Text>
-          </Box>
-          <Box pt={10}>
-            <Text fontWeight={600} fontSize={24}>
+              and protect what&apos;s most important to your organization.{' '}
               Enabled by <></>Accountable Threshold Signatures with Proactive
               Refresh.
-            </Text>
-            <Text fontSize={20}>
-              Accountable Threshold Signatures with Proactive Refresh were first
-              described by Dan Boneh, cryptography researcher and professor at
-              Stanford University. It is a way for a quorum of members of an
-              organization to securely sign messages. Unlike previous secret
-              sharing schemes, the scheme we implement refreshes signers&apos;
-              secrets once every 30 seconds, ensuring that an adversary must
-              compromise all signers&apos; keys within a 30-second window in
-              order to mount a successful attack. This is significantly more
-              secure than previous signature schemes.
             </Text>
           </Box>
           <HStack gap={3} fontSize={20} justifyContent='center' py={8}>
@@ -68,6 +53,19 @@ export default function Home() {
           textAlign='center'
         >
           {`Our safe ->`}
+        </Box>
+        <Box pt={10}>
+          <Text fontSize={20}>
+            Accountable Threshold Signatures with Proactive Refresh were first
+            described by Dan Boneh, cryptography researcher and professor at
+            Stanford University. It is a way for a quorum of members of an
+            organization to securely sign messages. Unlike previous secret
+            sharing schemes, the scheme we implement refreshes signers&apos;
+            secrets once every 30 seconds, ensuring that an adversary must
+            compromise all signers&apos; keys within a 30-second window in order
+            to mount a successful attack. This is significantly more secure than
+            previous signature schemes.
+          </Text>
         </Box>
       </main>
     </div>
