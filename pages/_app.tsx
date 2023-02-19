8f02fce7a43bf65c0a66da668bf8cef77f37d7a3
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2023-02-18 16:52:17 -0800

    landing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,19 +7,55 @@ import { SafeThemeProvider } from "@safe-global/safe-react-components";
 import type { AppProps } from "next/app";
 import { WagmiConfig } from "wagmi";
 import { wagmiClient } from "@/config/wagmi";
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "black",
+      },
+      a: {
+        color: 'blue.700',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+      h1: {
+        fontSize: '4xl',
+        fontWeight: 'bold',
+      },
+      h2: {
+        fontSize: '2xl',
+        fontWeight: 'bold',
+      },
+      h3: {
+        fontSize: 'xl',
+        fontWeight: 'bold',
+      },
+      h4: {
+        fontSize: 'lg',
+        fontWeight: 'bold',
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <SafeProvider>
-        <SafeThemeProvider mode="light">
-          {(safeTheme: Theme) => (
-            <ThemeProvider theme={safeTheme}>
-              <Component {...pageProps} />
-            </ThemeProvider>
-          )}
-        </SafeThemeProvider>
-      </SafeProvider>
-    </WagmiConfig>
+    <ChakraProvider theme={theme}>
+      <WagmiConfig client={wagmiClient}>
+        <SafeProvider>
+          <SafeThemeProvider mode="dark">
+            {(safeTheme: Theme) => (
+              <ThemeProvider theme={safeTheme}>
+                <Component {...pageProps} />
+              </ThemeProvider>
+            )}
+          </SafeThemeProvider>
+        </SafeProvider>
+      </WagmiConfig>
+    </ChakraProvider>
   );
 }
